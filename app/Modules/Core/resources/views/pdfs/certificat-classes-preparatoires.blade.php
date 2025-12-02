62f1c9a4f6a1eaec386586f6c5be0a3e9a074732
fadelsew02 <sewadefadel@gmail.com>
2025-12-02 12:35:21 +0100

    commit

--- app/Modules/Core/resources/views/pdfs/certificat-classes-preparatoires.blade.php
+++ app/Modules/Core/resources/views/pdfs/certificat-classes-preparatoires.blade.php
@@ -1,177 +1,220 @@
-@extends('core::pdfs.epac-base')
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Certificat Préparatoire</title>
+    <style>
+        @font-face {
+            font-family: "ALGERIA";
+            src: url({{ storage_path('fonts/ALGERIA.ttf') }});
+        }
+        @font-face {
+            font-family: "Arial";
+            src: url({{ storage_path('fonts/arial.ttf') }});
+        }
+        @font-face {
+            font-family: "Albertus Medium";
+            src: url({{ storage_path('fonts/albr55w.ttf') }});
+        }
+        @font-face {
+            font-family: 'Pristina';
+            src: url({{ storage_path('fonts/PRISTINA.ttf') }}) format('truetype');
+        }
+        @font-face {
+            font-family: "Berlin Sans FB";
+            src: url({{ storage_path('fonts/Berlin Sans FB Regular.ttf') }});
+        }
 
-@section('title', 'Attestation PDF')
-
-@section('body-font-size', '14pt')
-@section('body-font-weight', 'normal')
-@section('body-margin', '5cm 2.5cm 2cm 2.5cm')
-
-@section('font-faces')
-    @font-face {
-        font-family: "ALGERIA";
-        src: url({{ storage_path('fonts/ALGERIA.ttf') }});
-    }
-    @font-face {
-        font-family: "Arial";
-        src: url({{ storage_path('fonts/arial.ttf') }});
-    }
-    @font-face {
-        font-family: "Albertus Medium";
-        src: url({{ storage_path('fonts/albr55w.ttf') }});
-    }
-    @font-face {
-        font-family: 'Pristina';
-        src: url({{ storage_path('fonts/PRISTINA.ttf') }}) format('truetype');
-    }
-    @font-face {
-        font-family: "Berlin Sans FB";
-        src: url({{ storage_path('fonts/Berlin Sans FB Regular.ttf') }});
-    }
-@endsection
-
-@section('extra-styles')
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-    body {
-        text-align: center;
-        font-family: "Albertus Medium";
-    }
-    .top {
-        height: 2.75cm;
-        border-radius: 1px solid black;
-        text-align: left;
-    }
-    .attestation {
-        font-size: 35pt;
-        font-family: 'ALGERIA';
-    }
-    .diplome {
-        font-size: 10pt;
-        font-weight: bold;
-        margin-bottom: 13px;
-        letter-spacing: -1px;
-    }
-    .main {
-        text-align: justify;
-        margin-top: 20px;
-    }
-    .first {
-        margin-bottom: 9px;
-        font-style: italic;
-        font-size: 13pt;
-    }
-    .mention {
-        text-decoration: underline;
-        font-weight: normal;
-    }
-    .retrait {
-        margin-left: 10mm;
-    }
-    .info {
-        margin-top: 5px;
-    }
-    .info p {
-        margin-top: 2px;
-        margin-bottom: 2px;
-    }
-    .filiere {
-        text-transform: uppercase;
-        color: rgba(36, 88, 187, 1);
-        font-size: 19pt;
-        font-family: 'Berlin Sans FB';
-        margin-top: 15px;
-        margin-bottom: 15px;
-        text-align: center;
-    }
-    .text {
-        margin-bottom: 4px;
-    }
-    .avant {
-        font-family: 'Berlin Sans FB';
-    }
-    .decision {
-        color: blue;
-        opacity: .8;
-        font-style: normal;
-        font-size: 13pt;
-    }
-    .directeur {
-        margin-top: 30px;
-        text-align: center;
-        padding-left: 42%;
-    }
-    .name {
-        font-size: 13pt;
-        font-style: italic;
-    }
-    .paragraph-pristina {
-        font-size: 14pt;
-        font-family: 'Pristina';
-        letter-spacing: 1.3px;
-    }
-@endsection
-
-@section('custom-header')
-<div style="text-align: center; margin-bottom: 30px; page-break-inside: avoid;">
-    <p style="font-size: 28pt; margin: 20px 0; text-transform: uppercase; " class="attestation">
-        CERTIFICAT PREPARATOIRE<br>AUX ETUDES D'INGENIEUR
-</p>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        
+        body {
+            font-family: "Albertus Medium";
+            font-size: 14pt;
+            margin: 0;
+            text-align: center;
+        }
+        
+        .header {
+            padding: 1cm 1cm;
+        }
+        
+        .content {
+            margin: 0 2cm;
+        }
+        
+        table {
+            border-collapse: collapse;
+        }
+        
+        table, td {
+            border: none;
+        }
+        
+        .attestation {
+            font-size: 28pt;
+            font-family: 'ALGERIA';
+            text-transform: uppercase;
+            margin: 22px 0 0;
+        }
+        
+        .main {
+            text-align: justify;
+            margin-top: 20px;
+        }
+        
+        .retrait {
+            margin-left: 10mm;
+        }
+        
+        .info {
+            margin-top: 5px;
+        }
+        
+        .info p {
+            margin: 2px 0;
+        }
+        
+        .filiere {
+            text-transform: uppercase;
+            color: rgba(36, 88, 187, 1);
+            font-size: 19pt;
+            font-family: 'Berlin Sans FB';
+            margin: 15px 0;
+            text-align: center;
+        }
+        
+        .directeur {
+            margin-top: 30px;
+            text-align: center;
+            padding-left: 42%;
+        }
+        
+        .first {
+            margin-bottom: 9px;
+            font-style: italic;
+            font-size: 13pt;
+        }
+        
+        .name {
+            font-size: 13pt;
+            font-style: italic;
+        }
+        
+        .paragraph-pristina {
+            font-size: 14pt;
+            font-family: 'Pristina';
+            letter-spacing: 1.3px;
+        }
+    </style>
+</head>
+<body>
     @php
-        $bannerPath = public_path('assets/banner-1.png');
+        $epacLogo = public_path('assets/epac.png');
+        $uacLogo  = public_path('assets/uac.jpeg');
     @endphp
-    @if(file_exists($bannerPath))
-    <img src="{{ $bannerPath }}" alt="Banner" style="max-width: 100%; height: auto;">
-    @endif
-</div>
-@endsection
 
-@section('hide-footer', true)
+    <div class="header">
+        {{-- Entête --}}
+        <table style="width: 100%;" >
+            <tr>
+                <td style="width: 20%; text-align: left;">
+                    @if(file_exists($epacLogo))
+                        <img src="{{ $epacLogo }}" alt="EPAC" style="height: 100px;">
+                    @endif
+                </td>
+                <td style="width: 60%; text-align: center; font-size: 10pt; line-height: 1.2;">
+                    <div style="font-weight: bold; text-transform: uppercase;">
+                        REPUBLIQUE DU BENIN
+                    </div>
+                    <hr style="border: 1.3px solid #000; margin: 5px auto; width: 80px;">
+                    <div style="font-weight: bold; text-transform: uppercase; margin-top: 3px;">
+                        UNIVERSITE D'ABOMEY - CALAVI
+                    </div>
+                    <div style="font-weight: bold; font-size: 1.1rem; text-transform: uppercase; margin-top: 3px;">
+                        ECOLE POLYTECHNIQUE D'ABOMEY-CALAVI
+                    </div>
+                    <hr style="border: 2px solid #000; margin: 5px auto; width: 150px;">
+                    <div style="margin-top: 6px; font-size: 1.1rem; font-style: italic;">
+                        DIRECTION
+                    </div>
+                </td>
+                <td style="width: 20%; text-align: right;">
+                    @if(file_exists($uacLogo))
+                        <img src="{{ $uacLogo }}" alt="UAC" style="height: 100px;">
+                    @endif
+                </td>
+            </tr>
+        </table>
 
-@section('content')
-<div class="main">
-    <p class="paragraph-pristina">
-        <span class="retrait">Le</span> Directeur de l'Ecole Polytechnique d'Abomey-Calavi
-        (EPAC), ex-Collège Polytechnique Universitaire (CPU), soussigné, atteste que:
-    </p>
-    <br/>
-    <div class="info">
-        <p style="margin-bottom: 5px;">
-            <span style="{{ $etudiant->genre == 'M' ? '' : 'text-decoration: line-through;' }}">Mr</span> / <span style="{{ $etudiant->genre == 'F' ? '' : 'text-decoration: line-through;' }}">Mlle</span>
-            <span style="text-transform: uppercase;">{{ $etudiant->nom }}</span>
-            <span style="text-transform: capitalize;"> {{ $etudiant->prenoms }} </span> .........................
-        </p> 
-        <p style="margin-bottom: 5px;">
-            Né<span>{{ $etudiant->genre == 'M' ? '' : 'e' }}</span>
-            {{ $etudiant->ne_vers == 0 ? 'le ' : '' }}<span class="date">{{ $etudiant->date_naissance }}</span> à
-            <span class="lieu" style="text-transform:capitalize;">{{ $etudiant->lieu_naissance }} (Rep. {{ $etudiant->pays_naissance }})</span>................
-        </p>
-        <p>
-            <!-- @if(trim(str_replace("-","",$etudiant->matricule))) Numéro matricule : {{ $etudiant->matricule }}<br>@endif -->
-            a obtenu le Certificat Préparatoire aux Etudes d'Ingénieur conformément à la délibération du {{ $etudiant->date_soutenance }}:
-        </p>
-    </div>
-    <div class="filiere">FILIERE : {{ trim($etudiant->filiere->libelle) }}</div>
+        <hr style="border: 1px solid #000; margin: 5px 0 8px;">
 
-    <div class="paragraph-pristina" style="margin-top: 15px;">
-        <span class="retrait">Le</span> présent certificat, revêtu du sceau de l'EPAC, est délivrée pour servir et valor ce que de droit.
-    </div>
-    <br/><br/>
-    <div class="directeur">
-        <p class="first" style="text-transform: capitalize;">{{ $signataire->poste }},</p>
-        <div style="height: 55px;"></div>
-        <p style="text-decoration: underline;">
-            <strong class="name">{{ $signataire->nomination }}</strong>
+        <table style="width: 100%; font-size: 9pt;">
+            <tr>
+                <td style="width: 50%; text-align: left;  margin: 5px 0">
+                    N° ....................................... /EPAC/ CAP/ UAC
+                </td>
+                <td style="width: 50%; text-align: right;  margin: 5px 0">
+                    Abomey-Calavi, le .....................................
+                </td> 
+            </tr>
+        </table>
+
+        {{-- Titre --}}
+        <br/>
+        <br/><br/>
+        <p class="attestation">
+            CERTIFICAT PREPARATOIRE<br>AUX ETUDES D'INGENIEUR
         </p>
     </div>
-    
-    <div style="position: fixed; bottom: 60px; text-align: center;">
-        <hr style="border: 1px solid black;">
-        <p style="font-size: 9pt; font-weight: bold;">
-            Ce certificat est le résultat de la mise à niveau de l'étudiant et n'est valable que pour une inscription aux études ingénieurs du CAP
+
+    <div class="content">
+        {{-- Contenu --}}
+    <div class="main">
+        <p class="paragraph-pristina">
+            <span class="retrait">Le</span> Directeur de l'Ecole Polytechnique d'Abomey-Calavi
+            (EPAC), ex-Collège Polytechnique Universitaire (CPU), soussigné, atteste que:
         </p>
+        <br/>
+        <div class="info">
+            <p style="margin-bottom: 5px;">
+                <span style="{{ $etudiant->genre == 'M' ? '' : 'text-decoration: line-through;' }}">Mr</span> / <span style="{{ $etudiant->genre == 'F' ? '' : 'text-decoration: line-through;' }}">Mlle</span>
+                <span style="text-transform: uppercase;">{{ $etudiant->nom }}</span>
+                <span style="text-transform: capitalize;"> {{ $etudiant->prenoms }} </span> .........................
+            </p> 
+            <p style="margin-bottom: 5px;">
+                Né<span>{{ $etudiant->genre == 'M' ? '' : 'e' }}</span>
+                {{ $etudiant->ne_vers == 0 ? 'le ' : '' }}<span class="date">{{ $etudiant->date_naissance }}</span> à
+                <span class="lieu" style="text-transform:capitalize;">{{ $etudiant->lieu_naissance }} (Rep. {{ $etudiant->pays_naissance }})</span>................
+            </p>
+            <p>
+                a obtenu le Certificat Préparatoire aux Etudes d'Ingénieur conformément à la délibération du {{ $etudiant->date_soutenance }}:
+            </p>
+        </div>
+        <div class="filiere">FILIERE : {{ trim($etudiant->filiere->libelle) }}</div>
+
+        <div class="paragraph-pristina" style="margin-top: 15px;">
+            <span class="retrait">Le</span> présent certificat, revêtu du sceau de l'EPAC, est délivrée pour servir et valor ce que de droit.
+        </div>
+        <br/><br/>
+        <div class="directeur">
+            <p class="first" style="text-transform: capitalize;">{{ $signataire->poste }},</p>
+            <div style="height: 55px;"></div>
+            <p style="text-decoration: underline;">
+                <strong class="name">{{ $signataire->nomination }}</strong>
+            </p>
+        </div>
+        
+        <div style="position: fixed; bottom: 60px; text-align: center;">
+            <hr style="border: 1px solid black;">
+            <p style="font-size: 9pt; font-weight: bold;">
+                Ce certificat est le résultat de la mise à niveau de l'étudiant et n'est valable que pour une inscription aux études ingénieurs du CAP
+            </p>
+        </div>
     </div>
-</div>
-@endsection
+</body>
+</html>
