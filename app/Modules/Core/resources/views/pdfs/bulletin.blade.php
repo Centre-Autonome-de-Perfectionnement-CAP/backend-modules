a15c765416286a9d515439271d18610c7ec3a568
Fadel SEWADE <117315461+fadelsew02@users.noreply.github.com>
2025-12-17 16:18:13 +0000

    Update bulletin.blade.php

--- app/Modules/Core/resources/views/pdfs/bulletin.blade.php
+++ app/Modules/Core/resources/views/pdfs/bulletin.blade.php
@@ -40,12 +40,16 @@
 
 @section('content')
 <div class="main" style="position: relative;">
-    <div style="position: absolute; top: -2px; left: 0;">
-        <img src="data:image/svg+xml;base64,{{ $qrcode }}" width="100px" height="100px" class="qrcode" alt="Code QR">
+    <div style="position: absolute; top: -5px; left: 0;">
+        @if(ucfirst($etudiant->genre) == 'Masculin')
+                        <img src="{{ storage_path('app/avatars/homme.png') }}" style="width: 80px; height: 80px;" alt="">
+                    @else
+                        <img src="{{ storage_path('app/avatars/femme.png') }}" style="width: 80px; height: 80px;" alt="">
+                    @endif
     </div>
     <div style="text-align: center; font-weight: bold; margin-bottom: 7px; font-size: 25px;">BULLETIN DE NOTES</div>
     <div style="text-align: center; font-weight: bold; margin-bottom: 20px; font-size: 15px;">Année Académique: {{ $annee }}</div>
-    <div style="position: absolute; top: -2px; right: 0;">
+    <div style="position: absolute; top: -5px; right: 0;">
         <img src="data:image/svg+xml;base64,{{ $qrcode }}" width="100px" height="100px" class="qrcode" alt="Code QR">
     </div>
     
